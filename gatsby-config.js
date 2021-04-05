60ce7a5233a96022427d5bfe139c71f3ff69788d
Omar Mohammad <omarmhmmd@Omars-MacBook-Pro.local>
2021-04-05 10:01:12 -0700

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  siteMetadata: {
+    title: "salah-times",
+  },
+  plugins: [
+    "gatsby-plugin-emotion",
+    "gatsby-plugin-image",
+    {
+      resolve: "gatsby-plugin-google-analytics",
+      options: {
+        trackingId: " G-ZL5B0MLFPF",
+      },
+    },
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "images",
+        path: "./src/images/",
+      },
+      __key: "images",
+    },
+  ],
+};
